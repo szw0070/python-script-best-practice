2ae1876dc66f50dadeadf877befeba9467272634
Sarah Wilson <szw0070@auburn.edu>
2021-03-22 16:09:47 -0500

    added doc string to function

--- smallest_factor.py
+++ smallest_factor.py
@@ -1,10 +1,26 @@
 #! /usr/bin/env python3
 
-# A script for getting the smallest prime factor of an integer.
+"A module for getting the smallest prime factor of an integer."
 
 import sys
 
 def get_smallest_prime_factor(n):
+    """
+    Returns the smallest integer that is a factor of 'n'.
+
+    If 'n' is a prime number 'None' is returned.
+
+    Parameters
+    ----------
+    n : int
+        The integer to be factored.
+
+    Returns
+    -------
+    int or None
+        The smallest integer that is a factor of 'n'
+        or None if 'n' is a prime.
+    """
     for i in range(2,n):
         if(n%i) == 0:
             return i
