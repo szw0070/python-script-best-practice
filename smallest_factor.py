b419667bfc3ec2679090438a19b296a272d058f9
Sarah Wilson <szw0070@auburn.edu>
2021-03-22 16:03:38 -0500

    working on assignment. code is running. added though if "__name__" == "__main__"

--- smallest_factor.py
+++ smallest_factor.py
@@ -4,19 +4,22 @@
 
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
-n = int(sys.argv[1])
-if n < 1:
-    sys.exit(sys.argv[0] + ": Expecting a positive integer")
+def get_smallest_prime_factor(n):
+    for i in range(2,n):
+        if(n%i) == 0:
+            return i
+    return None
 
-smallest_prime_factor = None
-for i in range(2, n):
-    if (n % i) == 0:
-        smallest_prime_factor = i
-        break
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
+    n = int(sys.argv[1])
+    if n < 1:
+        sys.exit(sys.argv[0] + ": Expecting a positive integer")
 
-if smallest_prime_factor is None:
-    print(n)
-else:
-    print(smallest_prime_factor)
+    smallest_prime_factor = get_smallest_prime_factor(n)
+
+    if smallest_prime_factor is None:
+        print(n)
+    else:
+        print(smallest_prime_factor)
